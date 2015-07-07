0c329d1be195e9329cb4425c250f0c05a38281da
Tj Gabbour <tg@4media-network.com>
2015-07-08 00:43:54 +0200

    More explanations

--- src/harmful_fun/core.clj
+++ src/harmful_fun/core.clj
@@ -1,20 +1,26 @@
-;; This is our namespace, "my-sketch.core".
+;; This is our namespace, "harmful-fun.core".
 ;; We can organize things in files, so we don't have huuuge files.
 (ns harmful-fun.core
+
   ;; Pull in stuff from a couple Quil namespace. Quil is what lets us draw.
   ;; For example, when you see "q/background" below, that's the function
   ;; "background" from the "quil.core" namespace.
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+
 (def screen-size 500)
 
 
 (defn setup []
+  ;; Fancy stuff someone put here to show possibilities.
+  (q/frame-rate 100)
+  (q/color-mode :rgb)
+
   ;; This is the initial state.
   {:x 100           ;; The ball's left/right position
    :y 150           ;; The ball's up/down position
-   :change-in-x 1}) ;; Go right this many pixels in each frame rate.
+   :change-in-x 1}) ;; Move right this many pixels in each frame rate.
 
 
 (defn update-state [state]
@@ -24,14 +30,15 @@
 
   ;; Do different things depending on how far right we are.
   (if (> (:x state) 485)
-    ;; Our ball just went past 485 pixels to the right.
+    ;; This only happens if our ball just went too far right.
     (assoc state
            ;; As usual, add :change-in-x units to the right.
            :x (+ (:x state)
                  (:change-in-x state))
-           ;; Make :change-in-x negative, so the ball goes left from now on.
+           ;; Move left. (Make :change-in-x negative.)
            :change-in-x -1)
-    
+
+    ;; The usual situation. The ball's moving in some direction.
     (assoc state
            ;; As usual, add :change-in-x units to the right.
            :x (+ (:x state)
@@ -39,27 +46,30 @@
            ;; :change-in-x stays in the same. (We can in fact delete this line.)
            :change-in-x (:change-in-x state))))
 
+
 (defn draw-state [state]  
-  (q/background 100)  ;; Set the canvas's background color.
-  (q/fill (q/color 100 250 200))  ;; Fill the next shape you draw with this color.
-  (q/ellipse (:x state) (:y state) 60 80))  ;; Draw ellipse with x/y/width/height.
+  (q/background 100)                       ;; Set the canvas's background color.
+  (q/fill (q/color 100 250 200))           ;; Fill the next shape drawn w/ this color.
+  (q/ellipse (:x state) (:y state) 60 80)) ;; Draw ellipse with x/y/width/height.
 
 
 (q/defsketch my-sketch
-  ;; Setup everything at first. Like screen size, initial state, etc.
+  ;; Setup screen size.
   :size [screen-size screen-size]
-  :setup setup
 
-  ;; These functions are called on each frame. Many times a second.
-  ;; Update state, then draw it. Over and over again.
-  :update update-state
-  :draw draw-state
+  ;; defsketch needs a :setup function, an :update function and a
+  ;; :draw function from us.
+  :setup setup          ;; Call our setup function only once, at the start.
+  :update update-state  ;; Call our update-state function many times a second.
+  :draw draw-state      ;; Call our draw-state right after update-state.
 
   ;; More setup.
   ;;
   ;; The canvas should be on top of every other window.
   :features [:keep-on-top]
 
-  ;; Make state flow through our functions. Each function gets it as
-  ;; input... then returns it as output.
+  ;; Make state flow through our functions:
+  ;; - setup function returns an initial state object
+  ;; - update-state gets a state object as input, then returns one
+  ;; - draw-state gets a state object as input, then returns one
   :middleware [m/fun-mode])
